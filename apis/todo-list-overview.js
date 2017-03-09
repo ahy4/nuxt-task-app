de8ef5395725b5905f102d20a647404e97621aed
ahyahya <mrfool38@gmail.com>
2017-03-10 00:56:04 +0900

    [from now] 2017/03/10 00:56:04
    diff --git a/apis/todo-list-overview.js b/apis/todo-list-overview.js
    new file mode 100644
    index 0000000..1229894
    --- /dev/null
    +++ b/apis/todo-list-overview.js
    @@ -0,0 +1,48 @@
    +const Router = require('koa-router');
    +const Promise = require('bluebird');
    +
    +module.exports = (TodoList, Todo) =>
    +
    +  new Router({ prefix: '/todo-list-overview' })
    +
    +    .get('/', async (ctx, next) => {
    +      const todoLists = await TodoList.find();
    +      const lists = await Promise.all(todoLists.map(async (todoList) => {
    +        const lid = todoList._id;
    +        const todos = await Todo.find({lid});
    +        return {
    +          name: todoList.name,
    +          todos
    +        };
    +      }));
    +      console.log(lists);
    +      const result = lists.map((list) => {
    +        // console.log('list:', list);
    +        const allCount = list.todos.length;
    +        const data = { allCount };
    +        if (allCount === 0) {
    +          data.hasChild = false;
    +          data.latestUpdate = Number.POSITIVE_INFINITY;
    +        } else {
    +          data.hasChild = true;
    +          data.completedCount = list.todos.filter(({completedCount}) => completedCount).length;
    +          data.latestUpdate = list.todos.reduce((todoA, todoB) => Math.max(todoA.createdAt, todoB.createdAt), {createdAt: 0});
    +          const now = +new Date();
    +          data.nearestDeadline = list.todos.reduce((todoA, todoB) => {
    +            return Math.abs(todoA.deadline - now) < Math.abs(todoB.deadline - now) ? todoA.deadline : todoB.deadline;
    +          });
    +        }
    +        return data;
    +      }).sort((a, b) => b.latestUpdate - a.latestUpdate).map((o) => {
    +        delete o.latestUpdate;
    +        return o;
    +      });
    +      console.log(result);
    +      ctx.body = JSON.stringify(result);
    +    });
    +
    +// TodoListからnameをぜんぶ取ってくる
    +// そのTodoList一つあたりについてるTodoを全部取ってくる
    +// Todoの個数、completedの個数を調べる
    +// 最新で追加した日付順にする
    +// 一番〆切が近いものを表示
    diff --git a/store/todo-list.js b/store/todo-list.js
    new file mode 100644
    index 0000000..d1c1fdc
    --- /dev/null
    +++ b/store/todo-list.js
    @@ -0,0 +1,7 @@
    +export const state = {
    +  list: []
    +};
    +
    +export const mutations = {
    +
    +};
    diff --git a/store/todo.js b/store/todo.js
    new file mode 100644
    index 0000000..e69de29

---
+++ apis/todo-list-overview.js
@@ -0,0 +1,48 @@
+const Router = require('koa-router');
+const Promise = require('bluebird');
+
+module.exports = (TodoList, Todo) =>
+
+  new Router({ prefix: '/todo-list-overview' })
+
+    .get('/', async (ctx, next) => {
+      const todoLists = await TodoList.find();
+      const lists = await Promise.all(todoLists.map(async (todoList) => {
+        const lid = todoList._id;
+        const todos = await Todo.find({lid});
+        return {
+          name: todoList.name,
+          todos
+        };
+      }));
+      console.log(lists);
+      const result = lists.map((list) => {
+        // console.log('list:', list);
+        const allCount = list.todos.length;
+        const data = { allCount };
+        if (allCount === 0) {
+          data.hasChild = false;
+          data.latestUpdate = Number.POSITIVE_INFINITY;
+        } else {
+          data.hasChild = true;
+          data.completedCount = list.todos.filter(({completedCount}) => completedCount).length;
+          data.latestUpdate = list.todos.reduce((todoA, todoB) => Math.max(todoA.createdAt, todoB.createdAt), {createdAt: 0});
+          const now = +new Date();
+          data.nearestDeadline = list.todos.reduce((todoA, todoB) => {
+            return Math.abs(todoA.deadline - now) < Math.abs(todoB.deadline - now) ? todoA.deadline : todoB.deadline;
+          });
+        }
+        return data;
+      }).sort((a, b) => b.latestUpdate - a.latestUpdate).map((o) => {
+        delete o.latestUpdate;
+        return o;
+      });
+      console.log(result);
+      ctx.body = JSON.stringify(result);
+    });
+
+// TodoListからnameをぜんぶ取ってくる
+// そのTodoList一つあたりについてるTodoを全部取ってくる
+// Todoの個数、completedの個数を調べる
+// 最新で追加した日付順にする
+// 一番〆切が近いものを表示
